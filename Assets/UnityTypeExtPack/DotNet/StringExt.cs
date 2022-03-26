33ccf452ea19a53bff4f0c9fadf2f8b607b90041
aillieo <aillieo@163.com>
2022-03-26 17:09:02 +0800

    update

--- Assets/UnityTypeExtPack/DotNet/StringExt.cs
+++ Assets/UnityTypeExtPack/DotNet/StringExt.cs
@@ -31,6 +31,15 @@ namespace AillieoUtils.TypeExt
             }
             return source.IndexOf(value, comparisonType) >= 0;
         }
-    }
 
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
 }
---
+++ Assets/UnityTypeExtPack/Unity/QuaternionExt.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace AillieoUtils.UnityTypeExt
+{
+
+    public static class QuaternionExt
+    {
+        public static Vector4 ToVector4(this Quaternion quaternion)
+        {
+            return new Vector4(quaternion.x, quaternion.y, quaternion.z, quaternion.w);
+        }
+
+        public static Quaternion FromVector4(Vector4 vector4)
+        {
+            return new Quaternion(vector4.x, vector4.y, vector4.z, vector4.w);
+        }
+    }
+}
